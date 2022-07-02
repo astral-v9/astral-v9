1a5530ea2f0304eb93ff8976e5fec88f6fc0fd83
Arizaki <79724270+Oreki-ASTRAL@users.noreply.github.com>
2022-07-03 00:19:05 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const bot_dev = require('@astral.js')
+const astral = require('astral.ts')
+
+config.astral = {
+token: "votre token",
+Prefix: "Votre prefix",
+Intent: "Astral.dev"
+}
+
+//Arrive prochainement...
